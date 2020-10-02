5ee3c3c1949808bc5aced7e2f47dc035d562f91c
Mead Alsorani <maadsorani24@gmail.com>
2020-10-02 20:36:57 +0300

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './Property/property-card/property-card.component';
+import { PropertyListComponent } from './Property/property-list/property-list.component';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      NavBarComponent,
+      PropertyCardComponent,
+      PropertyListComponent
+   ],
   imports: [
     BrowserModule
   ],
